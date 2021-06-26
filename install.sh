e27e39ab516a73c59e0a6b779341dc92f36ba9c0
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2021-06-26 16:24:08 +0700

    submodule

--- install.sh
+++ install.sh
@@ -10,6 +10,9 @@ del() {
 mov() {
     cp -r "$0" "$1" 2>/dev/null
 }
+sub_dl() {
+    git submodule update --init "$0"
+}
 
 del "$HOME/.config/nvim"
 del "$HOME/.vim"
@@ -19,8 +22,10 @@ mov ".config/nvim" "$HOME/.config/nvim"
 mov ".vim" "$HOME/"
 mov ".termux" "$HOME/"
 mov ".bashrc" "$HOME/"
-mov ".oh-my-bash" "$HOME/"
-mov ".ohmybashrc" "$HOME/.oh-my-bash/"
+sub_dl "fzf"
 mov ".fzf" "$HOME/"
 mov ".fzf.bash" "$HOME/"
+sub_dl "oh-my-bash"
+mov ".oh-my-bash" "$HOME/"
+mov ".ohmybashrc" "$HOME/.oh-my-bash/"
 echo "==== DONE ===="
